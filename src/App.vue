da1377a804f78ad3983e48d8b2a7c5d6dd17d635
hardhill <hardhill@mail.ru>
2021-09-03 13:40:09 +0800

    all

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld :coords="coords" :typemap="typemap" :zoom="zoom" :size="size"/>
 </template>
 
 <script>
@@ -10,6 +10,14 @@ export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data(){
+    return {
+      coords:[55.88,37.77],
+      typemap:'sat,skl',
+      zoom:10,
+      size:[480,320],
+    }
   }
 }
 </script>
